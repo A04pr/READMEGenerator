370916155f54474476ee29488135d3156abd8112
Aidan Reed <areed041704@gmail.com>
2024-02-02 16:22:00 -0500

    init

---
+++ Develop/index.js
@@ -0,0 +1,14 @@
+// TODO: Include packages needed for this application
+
+const questions = ['Enter a README title', 'Enter a README description', 'Enter README Table of Contents',
+'Enter README installation guide;', 'Enter README usage guide', 'Enter a license', 'Enter your Github username',
+'Enter your email adress'];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
